5301e95bce05418d4c230312ed1844c80d424c95
Luiz Henrique Araújo Dantas <luizhadantas@gmail.com>
2023-11-21 14:55:30 -0300

    v.1.1 - adicionado paralelismo nos arquivos e arquivo de entrada de dados

--- src/matrix.c
+++ src/matrix.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <omp.h>
+
+#define thread_count 4
 
 void matrix_mult(int n1, int n2, int n3, double **A, double **B, double **C)
 {
- int i, j, k;
- double sum = 0.0;
 
- for (i = 0; i<n1; i++)
- {
-  for (j = 0; j<n3; j++)
-  {
-   for (k = 0; k<n2; k++)
-   {
-    sum = sum + A[i][k]*B[k][j];
-   }
-   C[i][j] = sum;
-   sum = 0.0;
-  }
- }
-
-
+    int i, j, k;
+    double sum = 0.0;
+#pragma omp parallel private(i, j, k, sum) shared(A, B, C) num_threads(thread_count)
+    for (i = 0; i < n1; i++)
+    {
+        int tid = omp_get_thread_num();
+        printf("%d", tid);
+        for (j = 0; j < n3; j++)
+        {
+            for (k = 0; k < n2; k++)
+            {
+                sum = sum + A[i][k] * B[k][j];
+            }
+            C[i][j] = sum;
+            sum = 0.0;
+        }
+    }
 }
