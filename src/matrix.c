ba7354ce252c4776515a024ff7f76e3631b75970
Luiz Henrique Araujo Dantas <luizhadantas@gmail.com>
2023-11-22 18:14:31 -0300

    v.1.1 ajustes nos arquivos de execução

--- src/matrix.c
+++ src/matrix.c
@@ -10,19 +10,19 @@ void matrix_mult(int n1, int n2, int n3, double **A, double **B, double **C)
 
     int i, j, k;
     double sum = 0.0;
-#pragma omp parallel private(i, j, k, sum) shared(A, B, C) num_threads(thread_count)
-    for (i = 0; i < n1; i++)
-    {
-        int tid = omp_get_thread_num();
-        printf("%d", tid);
-        for (j = 0; j < n3; j++)
+#pragma omp parallel private(i, j, k) shared(A, B, C) firstprivate(sum){
+        #pragma omp for
+        for (i = 0; i < n1; i++)
         {
-            for (k = 0; k < n2; k++)
+            for (j = 0; j < n3; j++)
             {
-                sum = sum + A[i][k] * B[k][j];
+                for (k = 0; k < n2; k++)
+                {
+                    sum = sum + A[i][k] * B[k][j];
+                }
+                C[i][j] = sum;
+                sum = 0.0;
             }
-            C[i][j] = sum;
-            sum = 0.0;
         }
     }
 }
