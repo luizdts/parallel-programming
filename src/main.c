0fafa5cad28bb46a89f8c9e84a699bbc08de5d79
lyndon.jonhson.700 <lyndon.jonhson.700@ufrn.edu.br>
2023-11-26 12:25:56 -0300

    v1.2 paralelizando tudo

--- src/main.c
+++ src/main.c
@@ -6,19 +6,18 @@
 #include "svd.h"
 #include "matrix.h"
 
-// const int ndof = 300 * 100 * 7;
 const int ndof = 300 * 100 * 7;
 const int isnp = 250;
 
 //------------------------------------
 
-main()
-{
-    omp_set_num_threads(8);
+main() {
+
+    omp_set_num_threads(4);
 
     int i, j, k;
-
     FILE *f;
+
     double *X;
     X = (double *)malloc(isnp * ndof * sizeof(double));
 
@@ -26,11 +25,8 @@ main()
 
     f = fopen("snapshot_u1", "r");
     k = -1;
-
-    for (j = 0; j < isnp; j++)
-    {
-        for (i = 0; i < ndof; i++)
-        {
+    for (j = 0; j < isnp; j++) {
+        for (i = 0; i < ndof; i++) {
             k++;
             fscanf(f, "%lf \n", &X[k]);
         }
@@ -45,20 +41,13 @@ main()
     // allocate an "array of arrays" of int
     A = (double **)malloc(ndof * sizeof(double *));
 
-#pragma omp parallel for private(row)
-    for (row = 0; row < ndof; row++)
-    {
+    for (row = 0; row < ndof; row++) {
         A[row] = (double *)malloc(isnp * sizeof(double));
     }
 
     k = -1;
-
-#pragma omp parallel for private(i)
-    for (i = 0; i < ndof; i++)
-    {
-#pragma omp parallel for private(j) shared(A, X)
-        for (j = 0; j < isnp; j++)
-        {
+    for (i = 0; i < ndof; i++) {
+        for (j = 0; j < isnp; j++) {
             k++;
             A[i][j] = X[k];
         }
@@ -72,21 +61,16 @@ main()
     double **v;
     v = (double **)malloc(isnp * sizeof(double *));
 
-#pragma omp parallel for private(row)
-    for (row = 0; row < isnp; row++)
-    {
+    for (row = 0; row < isnp; row++) {
         v[row] = (double *)malloc(isnp * sizeof(double));
     }
 
-    printf("into svd -- start the counting here! \n");
     dsvd(A, ndof, isnp, w, v);
 
     printf("done with svd \n");
 
     f = fopen("w.dat", "w+");
-#pragma omp parallel for private(i)
-    for (i = 0; i < isnp; i++)
-    {
+    for (i = 0; i < isnp; i++) {
         fprintf(f, "%d %f\n", i, w[i]);
     }
     fclose(f);
@@ -99,18 +83,12 @@ main()
     // allocate an "array of arrays" of int
     AT = (double **)malloc(isnp * sizeof(double *));
 
-#pragma omp parallel for private(row)
-    for (row = 0; row < isnp; row++)
-    {
+    for (row = 0; row < isnp; row++) {
         AT[row] = (double *)malloc(ndof * sizeof(double));
     }
 
-#pragma omp parallel for private(i)
-    for (i = 0; i < ndof; i++)
-    {
-#pragma omp parallel for private(j) shared(A, AT)
-        for (j = 0; j < isnp; j++)
-        {
+    for (i = 0; i < ndof; i++) {
+        for (j = 0; j < isnp; j++) {
             AT[j][i] = A[i][j];
         }
     }
@@ -120,9 +98,7 @@ main()
     // allocate an "array of arrays" of int
     Iden = (double **)malloc(isnp * sizeof(double *));
 
-#pragma omp parallel for private(row)
-    for (row = 0; row < isnp; row++)
-    {
+    for (row = 0; row < isnp; row++) {
         Iden[row] = (double *)malloc(isnp * sizeof(double));
     }
 
@@ -132,12 +108,9 @@ main()
 
     double s1 = 0.0;
     double s2 = 0.0;
-#pragma omp parallel for private(i)
-    for (i = 0; i < isnp; i++)
-    {
-#pragma omp parallel for private(j)
-        for (j = 0; j < isnp; j++)
-        {
+
+    for (i = 0; i < isnp; i++) {
+        for (j = 0; j < isnp; j++) {
             if (i == j)
                 s1 += Iden[i][j];
             else
@@ -150,6 +123,5 @@ main()
 
     printf("sum of diagonals = %f \n", s1);
     printf("sum of off-diagonals = %f \n", s2);
-
     //------------------------------------
 }
