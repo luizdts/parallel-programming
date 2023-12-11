a9874cb1bfce2c8a2bf8bd1de01452f511473724
Luiz Henrique Araújo Dantas <luizhadantas@gmail.com>
2023-12-11 13:43:46 -0300

    code teste com erros.

--- src/matrix.c
+++ src/matrix.c
@@ -8,17 +8,22 @@ void matrix_mult(int n1, int n2, int n3, double **A, double **B, double **C)
     int i, j, k;
     double sum = 0.0;
 #pragma omp parallel private(i, j, k) shared(A, B, C) firstprivate(sum)
-#pragma omp for
-    for (i = 0; i < n1; i++)
     {
-        for (j = 0; j < n3; j++)
+
+#pragma omp for
         {
-            for (k = 0; k < n2; k++)
+            for (i = 0; i < n1; i++)
             {
-                sum = sum + A[i][k] * B[k][j];
+                for (j = 0; j < n3; j++)
+                {
+                    for (k = 0; k < n2; k++)
+                    {
+                        sum = sum + A[i][k] * B[k][j];
+                    }
+                    C[i][j] = sum;
+                    sum = 0.0;
+                }
             }
-            C[i][j] = sum;
-            sum = 0.0;
         }
     }
 }
