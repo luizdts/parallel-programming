64be53943fa1ff7d53b2ada7dcb1575cd35a7f8d
Luiz Henrique Araújo Dantas <luizhadantas@gmail.com>
2023-12-11 17:14:44 -0300

    new code with return and break commented.

--- src/matrix.c
+++ src/matrix.c
@@ -9,20 +9,17 @@ void matrix_mult(int n1, int n2, int n3, double **A, double **B, double **C)
     double sum = 0.0;
 #pragma omp parallel private(i, j, k) shared(A, B, C) firstprivate(sum)
     {
-
 #pragma omp for
+        for (i = 0; i < n1; i++)
         {
-            for (i = 0; i < n1; i++)
+            for (j = 0; j < n3; j++)
             {
-                for (j = 0; j < n3; j++)
+                for (k = 0; k < n2; k++)
                 {
-                    for (k = 0; k < n2; k++)
-                    {
-                        sum = sum + A[i][k] * B[k][j];
-                    }
-                    C[i][j] = sum;
-                    sum = 0.0;
+                    sum = sum + A[i][k] * B[k][j];
                 }
+                C[i][j] = sum;
+                sum = 0.0;
             }
         }
     }
